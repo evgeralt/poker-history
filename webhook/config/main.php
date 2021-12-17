b3a313d783f9bcbf786daaf7b95581f16b61add2
Evgeniy Michkaryov <michkarev.ev57@gmail.com>
2021-12-17 08:12:59 +0500

    initial commit

--- webhook/config/main.php
+++ webhook/config/main.php
@@ -27,12 +27,14 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-
+        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
+            'rules' => [
+            ],
         ],
-
+        */
     ],
     'params' => $params,
 ];
