edc7bb8e800ea548412c151e510bd9636b5234ba
Evgeniy Michkaryov <michkarev.ev57@gmail.com>
2021-12-17 08:11:57 +0500

    initial commit

--- webhook/controllers/SiteController.php
+++ webhook/controllers/SiteController.php
@@ -9,15 +9,6 @@ use yii\web\Controller;
 
 class SiteController extends Controller
 {
-    public function actions()
-    {
-        return [
-            'error' => [
-                'class' => 'yii\web\ErrorAction',
-            ],
-        ];
-    }
-
     public function actionSetHook()
     {
         /** @var TelegramBot $telegram */
