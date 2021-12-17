e4b8770addf817dd7a49ae2672121a5d58f3aef5
Evgeniy Michkaryov <michkarev.ev57@gmail.com>
2021-12-17 08:10:03 +0500

    initial commit

--- webhook/controllers/SiteController.php
+++ webhook/controllers/SiteController.php
@@ -4,6 +4,7 @@ namespace webhook\controllers;
 
 use evgeralt\yii2telegram\TelegramBot;
 use Yii;
+use yii\web\BadRequestHttpException;
 use yii\web\Controller;
 
 class SiteController extends Controller
@@ -17,13 +18,26 @@ class SiteController extends Controller
         ];
     }
 
-    public function actionIndex()
+    public function actionSetHook()
     {
         /** @var TelegramBot $telegram */
         $telegram = Yii::$app->telegram;
         $telegram->client->setWebhook(Yii::$app->params['webhookUrl']);
 
         return 'ok';
+    }
+
+    public function actionHook($id)
+    {
+        if ($id !== 'sadlkxcvuy234AS') {
+            return new BadRequestHttpException();
+        }
+
+        /** @var TelegramBot $telegram */
+        $telegram = Yii::$app->telegram;
+        $telegram->client->handle();
+
+        return '';
     }
 
     public function actionError()
