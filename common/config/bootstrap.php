67b4f962e98bbf9edf0827aafb35fa536c7b9718
Evgeniy Michkaryov <michkarev.ev57@gmail.com>
2021-12-17 08:02:31 +0500

    initial commit

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -1,5 +1,4 @@
 <?php
 Yii::setAlias('@common', dirname(__DIR__));
-Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
-Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
+Yii::setAlias('@webhook', dirname(dirname(__DIR__)) . '/webhook');
 Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
